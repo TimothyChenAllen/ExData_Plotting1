42bdef3b62cd0d73f23ea41167ceff9f22549b8a
Timothy Chen Allen <timothychenallen@gmail.com>
2015-07-11 17:27:40 -0400

    Finished cleanup and unit_test

--- cleanup.R
+++ cleanup.R
@@ -1,3 +1,5 @@
 # Clean up everything
+# Clean the environment
 rm(list=ls())
+# Close the current graphics device if necessary
 if (dev.cur() > 1) dev.off()
--- unit_test.R
+++ unit_test.R
@@ -1,3 +1,4 @@
+# Run this to clean the environment and create all plots
 source("cleanup.R")
 source("plot1.R")
 source("plot2.R")
